3d991d063071a92ce8f09216e448d1831a85e8cc
Evan Pitstick <emp@seclab.in>
2023-05-12 18:35:52 -0400

    ready for init deploy

--- undelete_bot.py
+++ undelete_bot.py
@@ -1,9 +1,20 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from time import sleep
 
 import praw
 import requests
 
-SUBREDDIT = 'TestForApp'
+logging.basicConfig(level=logging.INFO)
+
+nsfw_logger = logging.getLogger("nsfw")
+nsfw_handler = RotatingFileHandler("nsfw.low", maxBytes=2 ** 20, backupCount=1)
+nsfw_handler.setLevel(logging.INFO)
+nsfw_logger.addHandler(nsfw_handler)
+
+# SUBREDDIT = 'testingground4bots'
+SUBREDDIT = 'undelete'
+
 reddit = praw.Reddit('frontpagewatch')
 
 nsfw_but_not_porn = [
@@ -12,7 +23,7 @@ nsfw_but_not_porn = [
     'watchpeopledie',
     'GreatApes',
     'Gore',
-    'DarkNetMarkets'
+    'DarkNetMarkets',
 ]
 
 # Links that we have already posted to /r/undelete
@@ -52,7 +63,7 @@ def check_removals():
         over18 = subreddit.over18
 
         if over18 and subreddit_name.lower() not in nsfw_but_not_porn:
-            print('--POTENTIAL SUB:', subreddit_name, flush=True)
+            nsfw_logger.info(f'POTENTIAL SUB: {subreddit_name}: {submission.title}')
             continue
 
         if thread_id in posted_ids:
@@ -69,8 +80,8 @@ def check_removals():
             url = 'https://www.reddit.com{}'.format(submission.permalink)
 
             # A little ugly maybe...
-            title = '[#{0}|+{1}|{2}] {4}  [/r/{3}]'.format(index, score, number_of_comments, subreddit.display_name,
-                                                           '{}')
+            title = '[#{0}|+{1}|{2}] {4}  [/r/{3}]'.format(
+                index, score, number_of_comments, subreddit.display_name, '{}')
 
             if len(title) - 2 + len(post_title) > 300:
                 title = title.format(post_title[:300 - (len(title) - 2 + 3)] + '...')
@@ -78,10 +89,10 @@ def check_removals():
                 title = title.format(post_title)
 
             # Post to /r/undelete
-            reddit.subreddit(SUBREDDIT).submit(title=title, url=url)
-            print('----DELETED AND POSTED!!!!!----', url)
+            res = reddit.subreddit(SUBREDDIT).submit(title=title, url=url)
+            logging.info(f'----DELETED AND POSTED!!!!!---- {url}')
         else:
-            print('--NOT DELTED:', 'https://www.reddit.com{}'.format(submission.permalink))
+            logging.info('--NOT DELETED: https://www.reddit.com{}'.format(submission.permalink))
 
     ids = new_ids
     ids_list = new_ids_list
